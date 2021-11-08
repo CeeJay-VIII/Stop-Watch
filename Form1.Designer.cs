cedd9eccae884a75bd8d93d0a237202c74bf524d
CeeJay-VIII <92804255+CeeJay-VIII@users.noreply.github.com>
2021-11-08 14:51:47 +0200

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,110 @@
+﻿
+namespace Stop_Watch
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.lblOutput = new System.Windows.Forms.Label();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.btnPause = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.Timer = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // lblOutput
+            // 
+            this.lblOutput.AutoSize = true;
+            this.lblOutput.Font = new System.Drawing.Font("Engravers MT", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblOutput.Location = new System.Drawing.Point(18, 27);
+            this.lblOutput.Name = "lblOutput";
+            this.lblOutput.Size = new System.Drawing.Size(338, 75);
+            this.lblOutput.TabIndex = 0;
+            this.lblOutput.Text = "00:00:00";
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(81, 143);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(75, 23);
+            this.btnStart.TabIndex = 1;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // btnPause
+            // 
+            this.btnPause.Location = new System.Drawing.Point(197, 143);
+            this.btnPause.Name = "btnPause";
+            this.btnPause.Size = new System.Drawing.Size(75, 23);
+            this.btnPause.TabIndex = 2;
+            this.btnPause.Text = "Pause";
+            this.btnPause.UseVisualStyleBackColor = true;
+            this.btnPause.Click += new System.EventHandler(this.btnPause_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(138, 186);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(75, 23);
+            this.btnClear.TabIndex = 3;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // Timer
+            // 
+            this.Timer.Tick += new System.EventHandler(this.Timer_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LightSeaGreen;
+            this.ClientSize = new System.Drawing.Size(373, 235);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnPause);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.lblOutput);
+            this.Name = "Form1";
+            this.Text = "Stop Watch";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblOutput;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button btnPause;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Timer Timer;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Stop_Watch
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        int hours, minutes, seconds;
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            Timer.Start();
+        }
+
+        private void btnPause_Click(object sender, EventArgs e)
+        {
+            Timer.Stop();
+            if (!(hours == 0 && minutes == 0 && seconds == 0))
+                btnStart.Text = "Continue";
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            Timer.Stop();
+            btnStart.Text = "Start";
+            lblOutput.Text = "00:00:00";
+            hours = 0;
+            minutes = 0;
+            seconds = 0;
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            seconds++;
+            if (seconds > 60)
+            {
+                minutes++;
+                seconds = 0;
+            }
+            if (minutes > 60)
+            {
+                hours++;
+                minutes = 0;
+            }
+            string hrs, min, sec;
+            if (seconds < 10)
+                sec = $"0{seconds}";
+            else
+                sec = seconds.ToString();
+            if (minutes < 10)
+                min = $"0{minutes}";
+            else
+                min = minutes.ToString();
+            if (hours < 10)
+                hrs = $"0{hours}";
+            else
+                hrs = hours.ToString();
+            // Concatenate the Output
+            lblOutput.Text = $"{hrs}:{min}:{sec}";
+        }
+    }
+}
